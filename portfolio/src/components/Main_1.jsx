979c79fd332f8180ec238335aac039ccaf0b4c04
Am-pro17 <amirmohe24@gmail.com>
2024-11-28 10:53:15 +0300

    Just starting

---
+++ portfolio/src/components/Main_1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+
+
+function Main_1() {
+  return (
+    <div>Main_1</div>
+  )
+}
+
+export default Main_1
---
+++ portfolio/src/components/Main_2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+
+function Main_2() {
+  return (
+    <div>Main_2</div>
+  )
+}
+
+export default Main_2
+
